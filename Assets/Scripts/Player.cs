7025f8ea60598f4585404837d628c249f94c6444
RileySetser <setserr7@gmail.com>
2024-02-22 15:04:27 -0500

    added player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!IsOwner) return;
+
+        Vector3 moveDir = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W)) moveDir.z = -1f;
+        if (Input.GetKey(KeyCode.S)) moveDir.z = +1f;
+        if (Input.GetKey(KeyCode.A)) moveDir.x = +1f;
+        if (Input.GetKey(KeyCode.D)) moveDir.x = -1f;
+
+        float moveSpeed = 3f;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
     }
 }
