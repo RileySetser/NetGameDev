ee79116c804b5196528af159c480de7c37992afb
Setser <setserra@mail.uc.edu>
2024-04-09 16:28:26 -0400

    finished basic lobby logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,18 @@ using UnityEngine;
 
 public class Player : NetworkBehaviour
 {
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float jumpAmount = 70;
     private bool isMoving;
+    private bool isGrounded;
+    private float distToGround;
+    private Collider collider;
+    private void Start()
+    {
+        collider = GetComponent<Collider>();
+        distToGround = GetComponent<Collider>().bounds.extents.y;
+    }
+
     private void Update()
     {
         if (!IsOwner) return;
@@ -22,6 +33,26 @@ public class Player : NetworkBehaviour
         float rotateSpeed = 10;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rb.AddForce(Vector3.up * jumpAmount, ForceMode.Impulse);
+
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + new Vector3(0, 1f, 0), transform.TransformDirection(Vector3.down), out hit, distToGround + 0.1f))
+        {
+            Debug.DrawRay(transform.position + new Vector3(0, 1f, 0), transform.TransformDirection(Vector3.down) * hit.distance, Color.red, 3.0f);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position + new Vector3(0, 1f, 0), transform.TransformDirection(Vector3.down) * 1000, Color.blue, 3.0f);
+        }
+        return Physics.Raycast(transform.position + new Vector3(0, 1f, 0), transform.TransformDirection(Vector3.down), out hit, distToGround + 0.1f);
     }
 
     public bool IsMoving()
